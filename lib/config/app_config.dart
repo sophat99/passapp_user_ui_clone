20169219d0080b146bc84db0f6fd177dbb5343cb
Sophat Heang <sophatheang@Sophats-MacBook-Pro.local>
2021-11-29 17:56:46 +0700

    add configuration file

---
+++ lib/config/app_config.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppConfig {
+    static const primaryColor = Colors.deepOrange;
+    static const primaryGreyColor = Colors.grey;
+    static const appBarTextColor = Colors.white;
+}
