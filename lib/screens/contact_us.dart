e7350f9c33b70a28ec8468e81d94b295fcd7cba0
sophat99 <sophatmail2@gmail.com>
2021-12-09 12:08:26 +0700

    Split all ContactUs's widget from ContactUs screen

--- lib/screens/contact_us.dart
+++ lib/screens/contact_us.dart
@@ -1,92 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:passapp_clone/screens/contact_us_widgets.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-class ContactUsScreen extends StatefulWidget {
+class ContactUsScreen extends StatelessWidget {
   const ContactUsScreen({Key? key}) : super(key: key);
-
-  @override
-  State<ContactUsScreen> createState() => _ContactUsPageState();
-}
-
-class _ContactUsPageState extends State<ContactUsScreen> {
   static const textStyle = TextStyle(fontSize: 15, fontWeight: FontWeight.bold);
 
   @override
   Widget build(BuildContext context) {
-    return Flex(direction: Axis.vertical, children: [
-      Expanded(
-        flex: 1,
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: [
-              Wrap(
-                runSpacing: 5,
-                children: [
-                  const Text(
-                    "Call to company",
-                    style: textStyle,
-                  ),
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  const Text(
-                    "Email us",
-                    style: textStyle,
-                  ),
-                  const Divider(
-                    thickness: 1,
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        "Legal",
-                        style: textStyle,
-                      ),
-                      Icon(Icons.arrow_forward_ios_rounded),
-                    ],
-                  ),
-                  const Divider(
-                    thickness: 1,
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-      _goToFacebookIcon()
-    ]);
+      return const ContactUsWidgets();
   }
 }
 
-Widget _goToFacebookIcon() {
-  const String toLaunch = 'https://www.facebook.com/sophat777/';
-  Future<void> _launchUniversalLinkIos(String url) async {
-    if (await canLaunch(url)) {
-      final bool nativeAppLaunchSucceeded = await launch(
-        url,
-        forceSafariVC: false,
-        universalLinksOnly: true,
-      );
-      if (!nativeAppLaunchSucceeded) {
-        await launch(
-          url,
-          forceSafariVC: true,
-        );
-      }
-    }
-  }
-  return Expanded(
-    flex: 1,
-    child: Center(
-        child: IconButton(
-          icon: Image.asset("assets/facebook_icon.png"),
-          iconSize: 100,
-          onPressed: () {
-              _launchUniversalLinkIos(toLaunch);
-          },
-        )),
-  );
-}
